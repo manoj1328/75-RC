87aa774c6981503260476e17801f836e7cbf7d95
Manoj <manooj2813@gmail.com>
2026-01-25 01:06:13 +0530

    Test

---
+++ src/main/java/rc/coding/RC75/Rc75Application.java
@@ -0,0 +1,13 @@
+package rc.coding.RC75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rc75Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rc75Application.class, args);
+	}
+
+}
---
+++ src/test/java/rc/coding/RC75/Rc75ApplicationTests.java
@@ -0,0 +1,13 @@
+package rc.coding.RC75;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rc75ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
